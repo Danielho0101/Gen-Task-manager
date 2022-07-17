1002602b2a46783c3526be62077be16b3dde7b92
Samuel <kamshing2360@gmail.com>
2022-07-17 14:41:04 +0800

    add task area

---
+++ index.js
@@ -0,0 +1,12 @@
+const todo = document.getElementById('todoTitle');
+const doing = document.getElementById('doingTitle');
+const done = document.getElementById('doneTitle');
+
+todo.style.backgroundColor = "rgb(244, 208, 255)";
+doing.style.backgroundColor = "rgb(193, 112, 189)";
+done.style.backgroundColor = "rgb(126, 55, 141)";
+
+const changeTaskListBackground = (target) => {
+    const changeColor = document.getElementById('taskListArea');
+    changeColor.style.backgroundColor = target.style.backgroundColor;
+};
